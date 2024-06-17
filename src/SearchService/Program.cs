bd846880e24519b3271ba05e3cd282d55f62539e
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-18 01:17:33 +0530

    Made http communication resilient using policy handler

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,6 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -17,7 +18,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
 {
     await DbInitializer.InitDb(app);
 }
@@ -25,5 +28,12 @@ catch (Exception e)
 {
     Console.WriteLine(e);
 }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
