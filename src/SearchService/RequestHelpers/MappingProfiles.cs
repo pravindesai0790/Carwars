ce1d31bdba19e2eeacfd6afcbff26cb8bdeb1f85
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-18 14:17:02 +0530

    Added contracts and consumer to consume a message from the Service bus

---
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Contracts;
+
+namespace SearchService;
+
+public class MappingProfiles : Profile
+{
+    protected MappingProfiles()
+    {
+        CreateMap<AuctionCreated, Item>();
+    }
+}
