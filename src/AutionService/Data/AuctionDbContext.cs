99a00e6cb6b6918543decd1b500e24bcbb13a39d
pravindesai0790 <pravindesai0790@gmail.com>
2024-06-18 20:16:52 +0530

    Added a masstransit message outbox to support data persistence if service bus is offline or failed in AuctionService

--- src/AutionService/Data/AuctionDbContext.cs
+++ src/AutionService/Data/AuctionDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using AutionService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace AutionService.Data;
@@ -10,4 +11,13 @@ public class AuctionDbContext : DbContext
     }
 
     public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
 }
