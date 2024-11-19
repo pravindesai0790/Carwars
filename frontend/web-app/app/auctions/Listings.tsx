75e5d0b25d9ef16b080d0eeb24fb61015db8ef95
pravindesai0790 <pravindesai0790@gmail.com>
2024-11-19 19:37:39 +0530

    Added types to the project and pagination to our list

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,7 +1,9 @@
+import { Auction, PagedResult } from "@/types";
 import AuctionCard from "./AuctionCard";
+import AppPagination from "../components/AppPagination";
 
-async function getData() {
-    const res = await fetch('http://localhost:6001/search?pageSize=10');
+async function getData(): Promise<PagedResult<Auction>> {
+    const res = await fetch('http://localhost:6001/search?pageSize=4');
 
     if(!res.ok) throw new Error('Failed to fetch data');
 
@@ -12,10 +14,15 @@ export default async function Listings() {
     const data = await getData();
 
     return (
-        <div className='grid grid-cols-4 gap-6'>
-            {data && data.results.map((auction: any) => (
-                <AuctionCard auction={auction} key={auction.id} />
-            ))}
-        </div>
+        <>
+            <div className='grid grid-cols-4 gap-6'>
+                {data && data.results.map((auction) => (
+                    <AuctionCard auction={auction} key={auction.id} />
+                ))}
+            </div>
+            <div className='flex justify-center mt-4'>
+                <AppPagination currentPage={1} pageCount={data.pageCount} />
+            </div>
+        </>
     )
 }
