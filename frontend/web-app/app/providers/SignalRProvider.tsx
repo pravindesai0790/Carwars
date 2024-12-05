4b33e1178661100e160b533e956a02a10dd82038
pravindesai0790 <pravindesai0790@gmail.com>
2024-12-05 19:15:42 +0530

    Added custome toast for an auction created

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -5,18 +5,30 @@ import { ReactNode, useCallback, useEffect, useRef } from "react"
 import { useAuctionStore } from "../hooks/useAuctionStore"
 import { useBidStore } from "../hooks/useBidStore"
 import { useParams } from "next/navigation"
-import { Bid } from "@/types"
+import { Auction, Bid } from "@/types"
+import { User } from "next-auth"
+import toast from "react-hot-toast"
+import AuctionCreatedToast from "../components/AuctionCreatedToast"
 
 type Props = {
     children: ReactNode
+    user: User | null
 }
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
     /* eslint-disable @typescript-eslint/no-unused-vars */
     const connection = useRef<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
     const params = useParams<{id: string}>();
+
+    const handleAuctionCreated  = useCallback((auction: Auction) => {
+        if(user?.username !== auction.seller) {
+            return toast(<AuctionCreatedToast auction={auction} />, {
+                duration: 10000
+            })
+        }
+    }, [user?.username])
 
     const handleBidPlaced = useCallback((bid: Bid) => {
         if(bid.bidStatus.includes('Accepted')) {
@@ -42,12 +54,14 @@ export default function SignalRProvider({ children }: Props) {
         }
 
         connection.current.on('BidPlaced', handleBidPlaced);
+        connection.current.on('AuctionCreated', handleAuctionCreated);
 
         return () => {
-            connection.current?.off('BidPlaced', handleBidPlaced)
+            connection.current?.off('BidPlaced', handleBidPlaced);
+            connection.current?.off('AuctionCreated', handleAuctionCreated);
         }
 
-    }, [handleBidPlaced]);
+    }, [handleBidPlaced, handleAuctionCreated]);
 
     return (
         children
